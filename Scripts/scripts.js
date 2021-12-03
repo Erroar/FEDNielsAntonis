d8c1597707957f66f97ab37ebec57d5494c66dee
Erroar <31957850+Erroar@users.noreply.github.com>
2021-12-03 09:26:26 +0100

    Add files via upload

---
+++ Scripts/scripts.js
@@ -0,0 +1,8 @@
+var deButton = document.querySelector("nav button");
+
+deButton.addEventListener("click", toonMenu);
+
+function toonMenu() {
+    var deNav = document.querySelector("nav");
+    deNav.classList.toggle("menuOpen");
+}
